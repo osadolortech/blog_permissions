0d2a084151611d8cff6c44b80013fa8132adbd10
osadolortech <brightosadolor8@gmail.com>
2022-06-27 15:32:53 +0100

    updated SERIALZERS

---
+++ blogapi/serializations.py
@@ -0,0 +1,11 @@
+from dataclasses import fields
+from rest_framework import serializers
+from .models import BlogModel
+
+
+class BlogSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = BlogModel
+        fields = (
+            'id','author','title','published','content'
+        )
