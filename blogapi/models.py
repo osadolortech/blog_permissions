319076c3988e41584815da44e6bfd09ea44f74d7
osadolortech <brightosadolor8@gmail.com>
2022-06-27 15:25:02 +0100

    updated model

--- blogapi/models.py
+++ blogapi/models.py
@@ -1,3 +1,13 @@
+from turtle import title
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class BlogModel(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published = models.DateTimeField(default=timezone.now)
+
+    
