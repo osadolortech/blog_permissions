7ba20b6e599f4c246f574d02e22fc4f3b004b314
osadolortech <brightosadolor8@gmail.com>
2022-06-27 16:11:36 +0100

    updated views

---
+++ blogapi/urls.py
@@ -0,0 +1,8 @@
+from django import views
+from django.urls import path
+from .views import BlogList, BlogDetail
+
+urlpatterns = [
+    path('<int:pk>', BlogDetail.as_view(), name='BlogView'),
+    path('', BlogList.as_view(), name='BlogPost')
+]
--- blogapi/views.py
+++ blogapi/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogModel
+from .serializations import BlogSerializers
 
-# Create your views here.
+
+class BlogList(generics.ListCreateAPIView):
+    queryset = BlogModel.objects.all()
+    serializer_class = BlogSerializers
+
+
+class BlogDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogModel.objects.all()
+    serializer_class = BlogSerializers
