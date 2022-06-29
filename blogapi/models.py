132bb7f548bd5ba80c6e8e8cfc12849941bc7e60
osadolortech <brightosadolor8@gmail.com>
2022-06-29 16:06:06 +0100

    created a registerserializer, created a userserializer,updated views for registering and also getting users details,updated urls for registering and getting usersdetails

--- blogapi/models.py
+++ blogapi/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class BlogModel(models.Model):
-    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    author = models.ForeignKey(User,related_name='posts',on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_at = models.DateTimeField(auto_now_add=True)
